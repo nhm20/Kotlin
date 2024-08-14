3e92ac22a1aa12425223563be03d81f22468a481
nhm20 <nmo212182@gmail.com>
2024-08-14 13:42:19 +0530

    Keywords

---
+++ src/Main.kt
@@ -0,0 +1,12 @@
+fun main(args:Array<String>) {
+    var a=40
+    println(a)
+    var `try`=20
+    println(`try`)
+    val b=20
+    print(b)
+//    b=b+10
+    var ab=40
+    var ba:Int
+    println(ab)
+}
