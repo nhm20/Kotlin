10b5b74be1ec739a0b4d56e9e869c00edc63693d
nhm20 <nmo212182@gmail.com>
2024-08-14 13:43:10 +0530

    Constants

--- src/Main.kt
+++ src/Main.kt
@@ -1,12 +1,6 @@
 fun main(args:Array<String>) {
-    var a=40
-    println(a)
-    var `try`=20
-    println(`try`)
     val b=20
     print(b)
-//    b=b+10
-    var ab=40
-    var ba:Int
-    println(ab)
+    b=b+10
+
 }
