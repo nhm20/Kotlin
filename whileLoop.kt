d761d3b94c7f6a408ce76e914995461767209487
Mohan K <151464177+nhm20@users.noreply.github.com>
2024-08-28 09:05:19 +0530

    whileLoop

---
+++ whileLoop.kt
@@ -0,0 +1,30 @@
+fun main(){
+    //while loop
+    var i=5;
+    while(i>0){
+        print("$i ")
+        i--
+    }
+    println()
+
+    //do while
+    i=5;
+    do{
+        print("value: $i ")
+        i--;
+    }
+    while(i>0)
+    println()
+
+    i=1;
+    do{
+        print("value: $i ")
+        i++;
+    }
+    while(i<=5)
+    println()
+
+}
+
+
+
