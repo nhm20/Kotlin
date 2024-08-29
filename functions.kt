c3eb79dd8235359f50b84c88edbd18ed5f1f1af4
Mohan K <151464177+nhm20@users.noreply.github.com>
2024-08-29 09:37:24 +0530

    functions

---
+++ functions.kt
@@ -0,0 +1,21 @@
+fun demo(){
+    println("Hello World");
+}
+// fun sum(a:Int=10,b:Int=20){
+//      println(a+b)
+// }
+//fun sum(a:Int,b:Int){
+//    println(a+b)
+//}
+fun main(){
+    demo()
+    println("Bye")
+    demo2()
+    sum(20,50)
+//     sum()
+
+
+}
+fun demo2(){
+    println("Hello World");
+}
