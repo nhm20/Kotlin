de468a1b40fa52fc93b4c1a721c26de5d1aec201
Mohan K <151464177+nhm20@users.noreply.github.com>
2024-09-03 17:38:05 +0530

    Interface

---
+++ interface.kt
@@ -0,0 +1,23 @@
+interface Shape{
+    fun area():Double
+}
+open class Rectangle(val a:Double,val b:Double):Shape{
+    override fun area():Double{
+        return a*b;
+    }
+    open fun display() {
+        println("rectangle with sides $a and $b is ${area()}")
+    }
+}
+open class Square(side :Double):Rectangle(side,side){
+    override fun display() {
+        println("square with sides $a is ${area()}")
+    }
+}
+
+fun main(){
+    val rec=Rectangle(2.0,4.0);
+    rec.display()
+    val sq=Square(3.0);
+    sq.display()
+}
